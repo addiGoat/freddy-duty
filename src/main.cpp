0e4e75c66fb07c6e472be95153dbdefa6ba40fe2
addiGoat <calvinseikoblais@gmail.com>
2026-06-18 21:46:36 -0400

    added debug cursor

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include <raylib.h>
 #include "button.hpp"
 
+void DrawDebugCursor(Vector2 mousePos) {
+    DrawLine(
+            mousePos.x,
+            mousePos.y - 20,
+            mousePos.x,
+            mousePos.y + 20,
+            BLACK
+            );
+
+    DrawLine(
+            mousePos.x - 20,
+            mousePos.y,
+            mousePos.x + 20,
+            mousePos.y,
+            BLACK
+            );
+
+    int x = mousePos.x;
+    int y = mousePos.y;
+    DrawText(
+            TextFormat("X: %i, Y: %i", x, y),
+            40, 40, 40, BLACK
+            );
+}
 
 int main() {
-    InitWindow(1280, 720, "freddy-duty");
-    SetTargetFPS(60);
+    int windowWidth = 1280;
+    int windowHeight = 720;
+    InitWindow(windowWidth, windowHeight, "freddy-duty");
+    SetTargetFPS(120);
 
     int currentCamera = 0;
 
@@ -21,7 +47,10 @@ int main() {
         ClearBackground(RAYWHITE);
 
         testButton.DrawButton();
-        DrawText(TextFormat("Camera: %i", currentCamera), 40, 40, 40, BLACK);
+        DrawText(TextFormat("Camera: %i", currentCamera),
+                windowWidth / 2, 40, 40, BLACK);
+
+        DrawDebugCursor(GetMousePosition());
         EndDrawing();
     }
 }
